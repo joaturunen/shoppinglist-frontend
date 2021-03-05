c9e4a770ce35873c39dc32539123f32433213a2b
Joanna Turunen <n0tujo00@students.oamk.fi>
2021-03-05 10:49:27 +0200

    valmis

--- src/App.js
+++ src/App.js
@@ -1,23 +1,115 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
 import './App.css';
 
+const URL = 'http://localhost/shoppinglist/';
+
 function App() {
+  const [items, setItems] = useState([]);
+  const [description, setDescription] = useState('');
+  const [amount, setAmount] = useState('');
+
+  useEffect(() => {
+    let status = 0;
+    fetch(URL + 'index.php')
+    .then(res => {
+      status = parseInt(res.status);
+      return res.json()
+    })
+    .then(
+      (res) => {
+        if (status === 200) {
+          setItems(res);
+        } else {
+          alert(res.error);
+        }
+      }, (error) => {
+        alert(error);
+      }
+    )
+  }, [])
+
+  function add(e) {
+    e.preventDefault();
+    let status = 0;
+    fetch(URL + 'add.php', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        description: description,
+        amount: amount
+      })
+    })
+    .then(res => {
+      status = parseInt(res.status);
+      return res.json();
+    })
+    .then(
+      (res) => {
+        if (status === 200) {
+          setItems(items => [...items, res]);
+          setDescription('');
+          setAmount('');
+        } else {
+          alert(res.error);
+        }
+      }, (error) => {
+        alert(error);
+      }
+    )
+  }
+
+  function remove(id) {
+    let status = 0;
+    fetch(URL + 'remove.php', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        id: id
+      })
+    })
+    .then(res => {
+      status = parseInt(res.status);
+      return res.json();
+    })
+    .then(
+      (res) => {
+        if (status === 200) {
+          const newListWithoutRemoved = items.filter((item) => item.id !== id);
+          setItems(newListWithoutRemoved);
+        } else {
+          alert(res.error);
+        }
+      }, (error) => {
+        alert(error);
+      }
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <h3>Shopping list</h3>
+      <div>
+        <form onSubmit={add}>
+          <label>New item</label>
+          <input value={description} onChange={e => setDescription(e.target.value)} placeholder="type description"/>
+          <input value={amount} onChange={e => setAmount(e.target.value)} placeholder="type amount"/>
+          <button>Add</button>
+        </form>
+      </div>
+
+      <div className="items">
+        <ol>
+          {items.map(item => (
+            <li key={item.id}><a>{item.description}</a> <a>{item.amount}</a> <a className="delete" onClick={() => remove(item.id)} href="#">Delete</a></li>
+          ))}
+        </ol>
+      </div>
     </div>
   );
 }
